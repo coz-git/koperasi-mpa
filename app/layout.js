a30c64972ef53b1594e6dde9708acab5abb60976
Kosasih Wahid Pramana <sultankosasih@gmail.com>
2023-04-04 10:00:22 +0700

    add prime layout

--- app/layout.js
+++ app/layout.js
@@ -2,23 +2,38 @@
 
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
+import { LayoutProvider } from '../context/layoutcontext';
 import AppBar from '../components/appbar';
+import './styles/layout/layout.scss';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 
 export const metadata = {
-  title: 'Koperasi MPA',
-  description: 'Created by Coz',
+    title: 'Koperasi MPA',
+    description: 'Created by Coz',
 };
 
+{
+    /* <LayoutProvider>
+    <Layout>
+        <Component {...pageProps} />
+    </Layout>
+</LayoutProvider>; */
+}
+
 export default function RootLayout({ children, session }) {
-  return (
-    <html lang="en">
-      <body>
-        <SessionProvider>
-          <AppBar />
-          <div className={'  h-screen '}>{children}</div>
-          {/* {children} */}
-        </SessionProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <SessionProvider>
+                    <LayoutProvider>
+                        {/* <AppBar /> */}
+                        <div className={'  h-screen '}>{children}</div>
+                        {/* {children} */}
+                    </LayoutProvider>
+                </SessionProvider>
+            </body>
+        </html>
+    );
 }
