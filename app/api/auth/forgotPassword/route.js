773f0a5c90cfb370097a4f8ba8453ebc926d2efb
Kosasih Wahid Pramana <sultankosasih@gmail.com>
2023-04-08 06:04:49 +0700

    add nodemailer

--- app/api/auth/forgotPassword/route.js
+++ app/api/auth/forgotPassword/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import DB from './../../../../lib/db';
+import transporter from '../../../../lib/transporter';
 
 export async function POST(req) {
   const reqData = await req.json();
@@ -11,5 +12,20 @@ export async function POST(req) {
     return NextResponse.json('User Not Found', { status: 400 });
   }
 
+  const mailData = {
+    to: 'blastemail17@gmail.com',
+    from: 'sultankosasih@gmail.com',
+    subject: `Message From Testing`,
+    text: 'Hi! | Sent from: Coz',
+    html: `<div>Hi!</div><p>Sent from:
+    Coz</p>`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else {
+      console.log(info);
+    }
+  });
   return NextResponse.json(user, { status: 200 });
 }
