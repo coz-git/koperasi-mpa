bfc03e9dcbca8bb8b16ec9b75bd33e400de89b3f
Kosasih Wahid Pramana <sultankosasih@gmail.com>
2023-05-23 21:00:53 +0700

    user add finish

--- app/admin/user/add/page.js
+++ app/admin/user/add/page.js
@@ -1,73 +1,118 @@
 'use client';
 
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import Link from 'next/link';
 import { Button } from 'primereact/button';
 import { InputText } from "primereact/inputtext";
 import { Dropdown } from 'primereact/dropdown';
 import Loading from './../../../../components/Loading';
+import { useSession } from "next-auth/react"
+import { Toast } from 'primereact/toast';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
+  const { data: sessionData, status : sessionStatus } = useSession()
+  const toast = useRef(null);
+  const router = useRouter();
   const [loading, setLoading] = useState(true)
   const [formData, setFormData] = useState({
-    nip : '',
-    nik : '',
-    name : '',
-    alamat : '',
-    telp : '',
-    role : '',
+    NIP : '',
+    NIK : '',
+    Name : '',
+    Email : '',
+    Alamat : '',
+    Telp : '',
+    Role : '',
   })
   const listRole = [
     { name: 'Admin', value: 'admin' },
     { name: 'User', value: 'user' }
   ];
 
-  const saveHandler = () => {
-    // console.log(first)
-    console.log(formData)
+  useEffect(() => {
+    if(sessionStatus != 'loading') {
+      setLoading(false)
+      // console.log(first)
+    }
+  }, [sessionStatus])
+
+  const saveHandler = async () => {
+    setLoading(true)
+
+    const res = await fetch(`/api/user`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionData.user.accessToken,
+      },
+      body: JSON.stringify(formData)
+    });
+
+    if(res.status == 400) {
+      toast.current.show({
+        severity: 'error',
+        summary: 'Error',
+        detail: res.statusText,
+      });
+    } 
+    else {
+      router.push('/admin/user')
+    }
+    setLoading(false);
   }
 
-  if (loading) { return <Loading isDashboard={true} /> } 
+  // if (loading) { return <Loading isDashboard={false} /> } 
   return (
     <>
-      <div className="flex items-center justify-between mb-4">
-        <div>
-          <h3 className="text-lg font-bold">USER MANAGEMENT</h3>
-        </div>
-        <div className="space-x-2">
-          <Link
-            className="text-sky-600 hover:text-sky-700"
-            href={'/admin/user'}
-          >
-          <Button label="Cancel" severity="danger" size="small" outlined />
-          </Link>
-          <Button label="Save" onClick={saveHandler} size="small" />
-        </div>
-      </div>
-      <div className="my-3">
-        <h5 className="font-bold mb-3">NIP</h5>
-          <InputText className="w-full" placeholder="Insert NIP" name="nip" value={formData.nip} onChange={(e) => setFormData({ ...formData, nip : e.target.value })} />
-      </div>
-      <div className="my-3">
-        <h5 className="font-bold mb-3">NIK</h5>
-          <InputText className="w-full" placeholder="Insert NIK" name="nik" value={formData.nik} onChange={(e) => setFormData({ ...formData, nik : e.target.value })} />
-      </div>
-      <div className="my-3">
-        <h5 className="font-bold mb-3">Name</h5>
-          <InputText className="w-full" placeholder="Insert Name" name="name" value={formData.name} onChange={(e) => setFormData({ ...formData, name : e.target.value })} />
-      </div>
-      <div className="my-3">
-        <h5 className="font-bold mb-3">Alamat</h5>
-          <InputText className="w-full" placeholder="Insert Alamat" name="alamat" value={formData.alamat} onChange={(e) => setFormData({ ...formData, alamat : e.target.value })} />
-      </div>
-      <div className="my-3">
-        <h5 className="font-bold mb-3">Telp</h5>
-          <InputText className="w-full" placeholder="Insert Telp" name="telp" value={formData.telp} onChange={(e) => setFormData({ ...formData, telp : e.target.value })} />
-      </div>
-      <div className="my-3">
-        <h5 className="font-bold mb-3">Role</h5>
-          <Dropdown value={formData.role}  onChange={(e) => setFormData({ ...formData, role : e.value })} options={listRole} optionLabel="name" placeholder="Select Role" className="w-full md:w-14rem" />
-      </div>
+    <Toast ref={toast} />
+    {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="flex items-center justify-between mb-4">
+            <div>
+              <h3 className="text-lg font-bold">USER MANAGEMENT</h3>
+            </div>
+            <div className="space-x-2">
+              <Link
+                className="text-sky-600 hover:text-sky-700"
+                href={'/admin/user'}
+              >
+              <Button label="Cancel" severity="danger" size="small" outlined />
+              </Link>
+              <Button label="Save" onClick={saveHandler} size="small" />
+            </div>
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">NIP</h5>
+              <InputText className="w-full" placeholder="Insert NIP" name="NIP" value={formData.NIP} onChange={(e) => setFormData({ ...formData, NIP : e.target.value })} />
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">NIK</h5>
+              <InputText className="w-full" placeholder="Insert NIK" name="NIK" value={formData.NIK} onChange={(e) => setFormData({ ...formData, NIK : e.target.value })} />
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">Name</h5>
+              <InputText className="w-full" placeholder="Insert Name" name="Name" value={formData.Name} onChange={(e) => setFormData({ ...formData, Name : e.target.value })} />
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">Email</h5>
+              <InputText className="w-full" placeholder="Insert Email" name="Email" value={formData.Email} onChange={(e) => setFormData({ ...formData, Email : e.target.value })} />
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">Alamat</h5>
+              <InputText className="w-full" placeholder="Insert Alamat" name="Alamat" value={formData.Alamat} onChange={(e) => setFormData({ ...formData, Alamat : e.target.value })} />
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">Telp</h5>
+              <InputText className="w-full" placeholder="Insert Telp" name="Telp" value={formData.Telp} onChange={(e) => setFormData({ ...formData, Telp : e.target.value })} />
+          </div>
+          <div className="my-3">
+            <h5 className="font-bold mb-3">Role</h5>
+              <Dropdown value={formData.Role}  onChange={(e) => setFormData({ ...formData, Role : e.value })} options={listRole} optionLabel="name" placeholder="Select Role" className="w-full md:w-14rem" />
+          </div>
+        </>
+      )}
     </>
   )
 }
--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server';
 import DB from '../../../lib/db';
 import jwt from 'jsonwebtoken';
+import transporter from '../../../lib/transporter';
+import fs from 'fs';
+import path from 'path';
+import * as handlebars from 'handlebars';
+import AES from 'crypto-js/aes';
 
 export async function GET(req) {
   const authHeader = req.headers.get("authorization");
@@ -38,18 +43,68 @@ export async function GET(req) {
 }
 
 export async function POST(req) {
-  const data = await req.json();
-  // console.log(res);
-  const user = await DB.Users.create({
-    data: {
-      NIK: data.NIK,
-      NIP: data.NIP,
-      Name: data.Name,
-      Alamat: data.Alamat,
-      Email: data.Email,
-      Telp: data.Telp,
-    },
-  });
+  const authHeader = req.headers.get("authorization");
+  
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    const data = await req;
+    // console.log(data)
 
-  return NextResponse.json('oke', { status: 200 });
+    try {
+      let decoded = jwt.verify(token, process.env.JWT_KEY);
+      if(decoded.Role == 'admin') {
+        const data = await req.json();
+
+        const user = await DB.Users.create({
+          data: {
+            NIK: data.NIK,
+            NIP: data.NIP,
+            Name: data.Name,
+            Alamat: data.Alamat,
+            Email: data.Email,
+            Telp: data.Telp,
+            Role : data.Role,
+          },
+        });
+
+        const ciphertext = AES.encrypt(user.Email, process.env.AUTH_SECRET);
+        const cryptToken = encodeURIComponent(ciphertext.toString());
+
+        const __dirname = path.resolve();
+        const filePath = path.join(__dirname, 'mail/register_user.html');
+        const source = fs.readFileSync(filePath, 'utf-8').toString();
+        const template = handlebars.compile(source);
+        const replacements = {
+          url_redirect: process.env.URL_PATH + '/auth/resetPassword/' + cryptToken,
+        };
+        const htmlToSend = template(replacements);
+
+        const mailData = {
+          to: user.Email,
+          from: '"MPA - Koperasi Team" <admin@mpa.com>',
+          envelope: {
+            from: process.env.MAIL_USER,
+            to: user.Email,
+          },
+          subject: `Message From Testing`,
+          html: htmlToSend,
+        };
+
+        return await transporter
+          .sendMail(mailData)
+          .then(() => {
+            return NextResponse.json('Email Reset Password already sended', {
+              status: 200,
+            });
+          })
+          .catch(() => {
+            return NextResponse.json('Something Wrong', { status: 400 });
+          });
+      }
+    } catch(err) {
+      return NextResponse.json('Something Wrong', { status: 400 });
+    }
+  }
+
+  return NextResponse.json('something wrong', { status: 400 });
 }
